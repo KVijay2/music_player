8d1e5a34e72620f9787c91f605b1050d865c80c2
KVijay2 <vijaykarunanidhi4@gmail.com>
2022-10-22 19:33:26 +0530

    first commit

---
+++ src/pages/AroundYou.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { useDispatch,useSelector } from 'react-redux';
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants'; 
+import { selectGenreListId } from '../redux/features/playerSlice';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
+
+const CountryTracks = () => {
+
+  const country = "IN";
+  const dispatch = useDispatch();
+  const [loading, _] = useState(true);
+  const { genreListId } = useSelector((state) => state.player);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+
+  if (isFetching && loading) return <Loader title="Loading Songs around you..." />;
+
+  if (error && country !== '') return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around you <span className="font-black">{country}</span></h2>
+
+      <select
+          onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+          value={genreListId || 'pop'}
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
+        </select>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CountryTracks;
